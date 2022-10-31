668d5839548a5715db7d6b2ea38d1e899b8bf30d
Realkayak <AarondAlexander@googlemail.com>
2022-10-31 11:23:03 +0000

    Add project files.

---
+++ DataAccessLibrary/MyInMemoryCacheComponent.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace DataAccessLibrary
+{
+    public class MyInMemoryCacheComponent
+    {
+        private readonly Dictionary<string, DateTime> _cacheEntryLastRecentlyUsed = new();
+        private readonly MemoryCache _memoryCache;
+        private readonly MemoryCacheOptions _cacheOptions = new();
+        private readonly double _entryAbsoluteExpirationInSeconds = 30;
+        private readonly int _entrySize = 1;
+        private int _cacheSizeLimit = 1;
+
+
+        public MyInMemoryCacheComponent(IMemoryCache memoryCache)
+        {
+            _memoryCache = (MemoryCache)memoryCache;
+        }
+
+        public int SetCacheSizeLimit { set
+            {
+                _cacheSizeLimit = value;
+
+                // set the maximum number of items; use a default value if none specified
+                _cacheOptions.SizeLimit = _cacheSizeLimit;
+            }
+        }
+
+        public void AddToCache<T>(string key, T value, int? entrySize, double? entryAbsoluteExpxirationInSeconds)
+        {
+            if(_memoryCache.Count == _cacheSizeLimit)
+            {
+                // check for 'least recently used' entries and remove as many entries as necessary
+                var listOfEntries = GetLeastRecentlyUsedEntries(entrySize ?? _entrySize);
+                if (listOfEntries != null)
+                {
+                    RemoveFromCache<T>(listOfEntries);
+                }
+            }            
+
+            double entryAbsExpInSeconds = entryAbsoluteExpxirationInSeconds ?? _entryAbsoluteExpirationInSeconds;
+
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                Size = entrySize ?? _entrySize
+                , AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(entryAbsExpInSeconds)
+            };
+
+            cacheEntryOptions.RegisterPostEvictionCallback(CacheEntryEvicted);
+
+            _memoryCache.Set<T>(key, value, cacheEntryOptions);
+
+            // update dictionary which stores LastRecentlyUsed data
+            _cacheEntryLastRecentlyUsed.Add(key, DateTime.UtcNow);
+        }
+
+        public bool TryGetValue<T>(string key, out T value)
+        {
+            value = default(T);
+
+            if(_memoryCache.TryGetValue(key, out T obj))
+            {
+                // object found in cache
+                value = obj;
+
+                // update dictionary which stores LastRecentlyUsed data
+                _cacheEntryLastRecentlyUsed[key] = DateTime.UtcNow;
+
+                return true;
+            }
+
+            // object not found in cache
+            return false;
+        }
+
+        private void RemoveFromCache<T>(List<string> entryKeys)
+        {
+            foreach (var entryKey in entryKeys)
+            {
+                _memoryCache?.Remove(entryKey);
+
+                // throw event to alert of cache entry removal
+                CacheItemEvictedEventArgs args = new();
+                args.EvictedKey = entryKey;
+
+                OnCacheItemEvicted(args);
+            }
+        }
+
+        private List<string>? GetLeastRecentlyUsedEntries(int entrySize)
+        {
+            // sort cache keys, by date in ascending order and get the number of items needing
+            var orderedEntryList = _cacheEntryLastRecentlyUsed?.OrderBy(x => x.Value).Take(entrySize).Select(x => x.Key).ToList();
+
+            return orderedEntryList;
+        }
+
+        public event EventHandler<CacheItemEvictedEventArgs>? CacheItemEvicted;
+
+        public class CacheItemEvictedEventArgs : EventArgs
+        {
+            public string? EvictedKey { get; set; }
+        }
+
+        protected virtual void OnCacheItemEvicted(CacheItemEvictedEventArgs e)
+        {
+            CacheItemEvicted?.Invoke(this, e);
+        }
+        
+        private void CacheEntryEvicted(object key, object value, EvictionReason reason, object state)
+        {
+            var rKey = key.ToString() ?? "";
+
+            switch (reason)
+            {
+                case EvictionReason.Removed:
+                    if (_memoryCache.TryGetValue(key.ToString(), out _) == false)
+                    {
+                        // update dictionary which stores LastRecentlyUsed data
+                        _cacheEntryLastRecentlyUsed?.Remove(rKey);
+                    }
+                    break;
+                case EvictionReason.Expired:
+                    // update dictionary which stores LastRecentlyUsed data
+                    _cacheEntryLastRecentlyUsed?.Remove(rKey);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ DataAccessLibrary/TestDataAccess.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class TestDataAccess
+    {
+        private readonly MyInMemoryCacheComponent _myInMemoryCache;
+
+        public TestDataAccess(MyInMemoryCacheComponent myInMemoryCache)
+        {
+            _myInMemoryCache = myInMemoryCache;
+            _myInMemoryCache.SetCacheSizeLimit = 3;
+        }
+
+        public async Task<string> AddRoleCacheEntry()
+        {
+            string added;
+
+            var cacheExists = _myInMemoryCache.TryGetValue<string>("role", out added);
+
+            if (!cacheExists)
+            {
+                added = "data";
+                await Task.Delay(3000);
+
+                _myInMemoryCache.AddToCache<string>("role", added, null, null);
+            }
+
+            return added;
+        }
+
+        public async Task<int> AddDepartmentCacheEntry()
+        {
+            int added;
+
+            var cacheExists = _myInMemoryCache.TryGetValue<int>("department", out added);
+
+            if (!cacheExists)
+            {
+                added = 1024;
+                await Task.Delay(3000);
+
+                _myInMemoryCache.AddToCache<int>("department", added, null, null);
+            }
+
+            return added;
+        }
+
+        public async Task<object> AddEmployeeCacheEntry()
+        {
+            object added;
+
+            var cacheExists = _myInMemoryCache.TryGetValue<object>("employee", out added);
+
+            if (!cacheExists)
+            {
+                added = new();
+                await Task.Delay(3000);
+
+                _myInMemoryCache.AddToCache<object>("employee", added, null, null);
+            }
+
+            return added;
+        }
+
+        public async Task<object> AddAccountCacheEntry()
+        {
+            object added;
+
+            var cacheExists = _myInMemoryCache.TryGetValue<object>("account", out added);
+
+            if (!cacheExists)
+            {
+                added = new();
+                    await Task.Delay(3000);
+
+                    _myInMemoryCache.AddToCache<object>("account", added, null, null);
+            }
+
+            return added;
+        }
+    }
+}
